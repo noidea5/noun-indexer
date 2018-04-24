a8ad9360e4643906ec1a175a53f42b31a93ff88d
noidea5 <32461629+noidea5@users.noreply.github.com>
2018-04-24 15:25:45 -0700

    Add files via upload

--- page-finder/PageTagger.java
+++ page-finder/PageTagger.java
@@ -3,9 +3,11 @@ package ner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PageTagger {
+	
 	
 	public static void main(String[] args) {
 		
@@ -16,8 +18,9 @@ public class PageTagger {
 		String inputFileContents = usingBufferedReader(inputFileAbsPath);
 //		System.out.println(inputFileContents);
 		
-		String regex = "\\d";//using regular expression to set what to identify, in this case numerical digits
-		String[][] page = {};//2D array for each number and its location
+		String regex = "\\d";//using regular expression to set what to identify, in this case numerical digit
+		ArrayList<Integer> page = new ArrayList<Integer>();//array for each number
+		ArrayList<Integer> location = new ArrayList<Integer>();//corresponding array for the number's location
 		
 		/* for loop runs through the entire .txt file to identify every number
 		 * first finds the initial digit, then checks to see if there are following digits
@@ -30,13 +33,15 @@ public class PageTagger {
 				while (inputFileContents.substring(counter, counter+1).matches(regex)) {
 					foundDigits = foundDigits + inputFileContents.substring(counter, counter+1);
 					counter++;
-//					System.out.println(foundDigits);
 				}
-//				TODO: add page number and position to array
-//				page.push(foundDigits, i);
-				System.out.println(foundDigits);
+				page.add(Integer.parseInt(foundDigits));
+				location.add(i);
+				
 				i = counter;
 			}
+		}
+		for(int i = 0; i < page.size(); i++) {
+			System.out.println(page.get(i) + ", " + location.get(i));
 		}
 	}
 	
@@ -58,5 +63,22 @@ public class PageTagger {
 	        e.printStackTrace();
 	    }
 	    return contentBuilder.toString();
+	}
+	
+	//method to get rid of all of the out of order numbers
+	public static void orderPages(ArrayList<Integer> page, ArrayList<Integer> location) {
+		ArrayList<Integer> outputPage = new ArrayList<Integer>();
+		ArrayList<Integer> outputLocation = new ArrayList<Integer>();
+		for (int i = 0; i < page.size(); i++) {
+			if (page.get(i) == page.get(i+1) - 1) {
+				System.out.println(page.get(i));
+				outputPage.add(page.get(i));
+				outputLocation.add(location.get(i));
+			} else {
+				page.remove(i);
+				location.remove(i);
+				i = i-1;
+			}
+		}
 	}
 }
